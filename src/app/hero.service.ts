968e87d9f8127d46263c2842b17bdbc1786fdc21
Raanan Adut <radesigner84@gmail.com>
2018-07-28 10:15:53 +0300

    fixing tabs

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { BehaviorSubject } from 'rxjs/internal/BehaviorSubject';
@@ -66,68 +65,3 @@ export class HeroService {
     return this.http.get( this.logoutUrl );
   }
 }
-=======
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { BehaviorSubject } from 'rxjs/internal/BehaviorSubject';
-
-@Injectable()
-export class HeroService {
-  sessionUrl = 'http://localhost:3000/users/session';
-  getCampaignUrl = 'http://localhost:3000/getallcampaigns';
-  getOneCampaignUrl = 'http://localhost:3000/getonecampaign';
-  getLeadsUrl = 'http://localhost:3000/getleads';
-  postCampaignUrl = 'http://localhost:3000/addcampaign';
-  loginUrl = 'http://localhost:3000/users/login';
-  logoutUrl = 'http://localhost:3000/users/logout';
-  deleteCampUrl = 'http://localhost:3000/deletecampaign';
-  private boolean = new BehaviorSubject<any>(true);
-  showBoolean = this.boolean.asObservable();
-  private user = new BehaviorSubject<string>('');
-  showUser = this.user.asObservable();
-  private campaignId = new BehaviorSubject<string>('');
-  onCampaignId = this.campaignId.asObservable();
-
-
-  constructor( private http: HttpClient ) {
-  }
-  openPopup( bool ) {
-    this.boolean.next( bool );
-  }
-
-  editUser( newUser ) {
-    this.user.next( newUser );
-  }
-  getClickedCampaignId( id ) {
-    this.campaignId.next( id );
-  }
-
-  getSession() {
-    return this.http.get( this.sessionUrl );
-  }
-  getLeads( id ) {
-    return this.http.get( this.getLeadsUrl + '/' + id );
-  }
-
-  getCampaign() {
-    return this.http.get( this.getCampaignUrl );
-  }
-  getOneCampaign( id ) {
-    return this.http.get( this.getOneCampaignUrl + '/' + id );
-  }
-  deleteCamp( id ) {
-    return this.http.delete( this.deleteCampUrl + '/' + id );
-  }
-  postCampign( campName ) {
-    return this.http.put( this.postCampaignUrl, campName );
-  }
-
-  postLogin( arg: object ) {
-    return this.http.post( this.loginUrl, arg );
-  }
-
-  logout() {
-    return this.http.get( this.logoutUrl );
-  }
-}
->>>>>>> 41d3ec41f7054c4cd65092d2d869d523602d7a37
--- src/app/popup/popup.component.ts
+++ src/app/popup/popup.component.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { Component, OnInit } from '@angular/core';
 import { HeroService } from '../hero.service';
 
@@ -38,44 +37,3 @@ export class PopupComponent implements OnInit {
   }
 }
 
-=======
-import { Component, OnInit } from '@angular/core';
-import { HeroService } from '../hero.service';
-
-@Component({
-  selector: 'app-popup',
-  template:
-  `
-  <div class="createCampaign">
-  <button (click)="closePopUp($event)" class="close"><i class="fa fa-times" aria-hidden="true"></i>
-  </button>
-    <h2>Create campaign</h2>
-    <input type="text" class="input-field" [(ngModel)]="campaignName" placeholder="Enter campaign's name">
-    <button class="form-button" (click)="sendCampaign()">Create</button>
-</div>
-  `,
-  styleUrls: ['./popup.component.css']
-})
-export class PopupComponent implements OnInit {
-  campaignName: String;
-  userObj: Object;
-  constructor( private service: HeroService ) { }
-
-  ngOnInit() {
-  }
-  async sendCampaign() {
-    this.userObj = {
-      campaignName: this.campaignName
-    };
-    await this.service.postCampign( this.userObj ).subscribe(( res: any ) => {
-      this.service.openPopup(false);
-    });
-  }
-  closePopUp(e) {
-    if (e.target.classList[0] === 'fa') {
-      this.service.openPopup(false);
-    }
-  }
-}
-
->>>>>>> 41d3ec41f7054c4cd65092d2d869d523602d7a37
